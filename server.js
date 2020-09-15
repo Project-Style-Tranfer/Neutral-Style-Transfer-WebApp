e3a198f1696bc37aa03e32ccb1cb3ad5095aae86
sulbhagarg <sulbhagarg99@gmail.com>
2020-09-15 15:20:13 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// =======================
+// Classic Node Packages
+// =======================
+global.express = require("express");
+global.app = express();
+global.path = require('path');
+
+// =======================
+// Environment Variables
+// =======================
+var PORT = 3000;
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+// ====================================
+// PORT listener, server is running :)
+// ====================================
+app.listen(PORT, function(){
+    console.log("The server is running...");
+})
